2013ebc571cdd672d59b1aea1f52baf64d952c7c
Fayz5153 <fayz5153@gmail.com>
2023-07-01 17:53:09 +0500

    verstka ready

--- js/script.js
+++ js/script.js
@@ -6,3 +6,29 @@ burger_btn.onclick = () =>{
   nav.classList.toggle("nav__show")
   burger_btn.classList.toggle("btn__close")
 }
+
+// Modal
+const modal_btn = document.querySelector("#modal__btn")
+const rm__close = document.querySelector(".rm__close")
+const modal__close = document.querySelector(".modal__close")
+const modal = document.querySelector(".result__modal")
+
+let winWidth = window.innerWidth
+
+modal_btn.onclick = () =>{
+  modal.classList.add("result__modal__open")
+  if (winWidth < 800) {
+    document.body.style = "overflow: hidden"
+  } else {
+    document.body.style = "overflow: hidden; width:calc(100% - 0.4em);"
+  }
+  
+}
+
+function modalClose() {
+  modal.classList.remove("result__modal__open")
+  document.body.style = "overflow: visible"
+}
+
+rm__close.onclick = modalClose
+modal__close.onclick = modalClose
